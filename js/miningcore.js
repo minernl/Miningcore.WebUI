eaba12a8eef33779d7534201ac204a22b95a893b
Matt Snoby <iotapi322@icloud.com>
2022-09-04 16:52:20 -0400

    you don't need the root url

--- js/miningcore.js
+++ js/miningcore.js
@@ -22,15 +22,6 @@
 
 
 
-// read WebURL from current browser
-var WebURL         = "http://ergotest.mattsnoby.com/";
-// WebURL correction if not ends with /
-if (WebURL.substring(WebURL.length-1) != "/")
-{
-	WebURL = WebURL + "/";
-	console.log('Corrected WebURL, does not end with / -> New WebURL : ', WebURL);
-}
-//var API            = WebURL + "api/";   						// API address is:  https://domain.com/api/
 var API            = "https://miningcore-api.mattsnoby.com/api/";
 // API correction if not ends with /
 if (API.substring(API.length-1) != "/")
